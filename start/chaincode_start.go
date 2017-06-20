a20d076962eb922c4ed5986c46e77b4333d46a19
Rohit Aneja <rohit.aneja@bcone.com>
2017-06-20 14:45:47 +0530

    updated chaincode

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -39,6 +39,7 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("Inside Init block ! Awesome")
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
