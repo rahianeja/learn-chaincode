87302c1d9a499ab4bf86f31a9faa4e392ec448fa
Rohit Aneja <rohit.aneja@bcone.com>
2017-06-27 14:53:30 +0530

    in progress query

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -97,7 +97,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		if err != nil {
 			return nil, err
 		}
-		return shim.Success("X value is set")
+		return Success("X value is set")
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
