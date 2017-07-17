cbca82a24957119c2adff40c0b6b840b8e7b7fcc
Rohit Aneja <rohit.aneja@bcone.com>
2017-07-17 14:35:54 +0530

    changing invoke

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -124,7 +124,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		if err != nil {
 		return nil, errors.New("Truckdata argument unmarshal failed: " + fmt.Sprint(err))
 		}
-
+		err =  stub.PutState("truckState", stateArg)
+		if err != nil {
+		return nil, errors.New("Truckstate argument putstate failed: " + fmt.Sprint(err))
+		}
 		return nil, nil
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
